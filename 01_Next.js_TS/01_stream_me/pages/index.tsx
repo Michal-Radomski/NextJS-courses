c88fbe5b48c2ab68d5185646db45adab28f560a9
Michal Radomski <michalra@op.pl>
2024-08-23 00:50:43 +0200

    Next.js with TS course - commit 05

--- 01_Next.js_TS/01_stream_me/pages/index.tsx
+++ 01_Next.js_TS/01_stream_me/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import { Inter } from "next/font/google";
 import { NextFont } from "next/dist/compiled/@next/font";
+import { Container, Typography, Box, Button } from "@material-ui/core";
+import Link from "next/link";
 
 const inter: NextFont = Inter({ subsets: ["latin"] });
 
@@ -13,7 +15,18 @@ export default function Home(): JSX.Element {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="./favicon.ico" />
       </Head>
-      <main className={""}>Next App</main>
+      <Container maxWidth="sm">
+        <Box my={4}>
+          <Typography variant="h4" component="h1" gutterBottom>
+            Next.js example
+          </Typography>
+          <Link href="/about">
+            <Button variant="contained" color="primary">
+              Go to the about page
+            </Button>
+          </Link>
+        </Box>
+      </Container>
     </>
   );
 }
