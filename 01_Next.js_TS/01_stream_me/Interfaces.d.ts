2972fece135e73861acd50c180b0f7ca1ad5be98
Michal Radomski <michalra@op.pl>
2024-08-29 02:56:03 +0200

    Next.js with TS course - commit 19

---
+++ 01_Next.js_TS/01_stream_me/Interfaces.d.ts
@@ -0,0 +1,3 @@
+declare module "*.graphqls";
+
+declare module "*.yml";
--- 01_Next.js_TS/01_stream_me/pages/_app.tsx
+++ 01_Next.js_TS/01_stream_me/pages/_app.tsx
@@ -1,26 +1,34 @@
 import React from "react";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import { ThemeProvider } from "@material-ui/core/styles";
-import { Props } from "next/script";
+import { useEffect, useState } from "react";
+import { ApolloProvider } from "@apollo/client";
 
+import { useApollo } from "lib/apollo";
 import { themeDark, themeLight } from "lib/theme";
 
-export default function MyApp(props: { Component: React.FC; pageProps: Props }): JSX.Element {
-  const { Component, pageProps } = props;
+export default function MyApp({ Component, pageProps }: { Component: React.FC; pageProps: any }): JSX.Element {
+  const [darkState, setDarkState] = useState(false);
+  const handleThemeChange = () => {
+    setDarkState(!darkState);
+  };
 
-  React.useEffect(() => {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+
+  useEffect(() => {
     // Remove the server-side injected CSS.
     const jssStyles = document.querySelector("#jss-server-side");
-    // console.log("jssStyles:", jssStyles);
     if (jssStyles && jssStyles.parentNode) {
       jssStyles.parentNode.removeChild(jssStyles);
     }
   }, []);
 
   return (
-    <ThemeProvider theme={true ? themeDark : themeLight}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <ApolloProvider client={apolloClient}>
+      <ThemeProvider theme={darkState ? themeDark : themeLight}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </ApolloProvider>
   );
 }
