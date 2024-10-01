9b5c87fb529f7c8d8322f90a225409478cbe27c1
Michal Radomski <michalra@op.pl>
2024-10-02 00:42:44 +0200

    NextJS course - commit 24

--- 02_NextJS/02_snippets/src/components/snippet-edit-form.tsx
+++ 02_NextJS/02_snippets/src/components/snippet-edit-form.tsx
@@ -2,7 +2,9 @@
 
 import React from "react";
 import Editor from "@monaco-editor/react";
+
 import type { Snippet as SnippetPrisma } from "@prisma/client";
+import * as actions from "@/actions";
 
 interface SnippetEditFormProps {
   snippet: SnippetPrisma | Snippet;
@@ -11,9 +13,12 @@ interface SnippetEditFormProps {
 export default function SnippetEditForm({ snippet }: SnippetEditFormProps): JSX.Element {
   const [code, setCode] = React.useState<string>(snippet.code);
 
-  const handleEditorChange = (value: string = "") => {
+  const handleEditorChange = (value: string = ""): void => {
+    // console.log("value:", value);
     setCode(value);
   };
+
+  const editSnippetAction: () => Promise<void> = actions.editSnippet.bind(null, snippet.id, code);
 
   return (
     <div>
@@ -26,6 +31,11 @@ export default function SnippetEditForm({ snippet }: SnippetEditFormProps): JSX.
         options={{ minimap: { enabled: true } }}
         onChange={handleEditorChange}
       />
+      <form action={editSnippetAction}>
+        <button type="submit" className="p-2 border rounded">
+          Save
+        </button>
+      </form>
     </div>
   );
 }
