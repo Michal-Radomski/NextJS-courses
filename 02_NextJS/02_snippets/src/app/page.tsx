9557ccf0644156314e1e807be0cb7d7231fbb73a
Michal Radomski <michalra@op.pl>
2024-10-01 23:44:17 +0200

    NextJS course - commit 20

--- 02_NextJS/02_snippets/src/app/page.tsx
+++ 02_NextJS/02_snippets/src/app/page.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/link";
 
 import { db } from "@/db";
 
@@ -6,12 +7,29 @@ export default async function Home(): Promise<React.JSX.Element> {
   const snippets: Snippet[] = await db.snippet.findMany();
 
   const renderedSnippets: JSX.Element[] = snippets.map((snippet: Snippet): JSX.Element => {
-    return <div key={snippet.id}>{snippet.title}</div>;
+    return (
+      <Link
+        key={snippet.id}
+        href={`/snippets/${snippet.id}`}
+        className="flex justify-between items-center p-2 border rounded"
+      >
+        <div>{snippet.title}</div>
+        <div>View</div>
+      </Link>
+    );
   });
 
   return (
     <React.Fragment>
-      <div>{renderedSnippets}</div>
+      <div>
+        <div className="flex m-2 justify-between items-center">
+          <h1 className="text-xl font-bold">Snippets</h1>
+          <Link href="/snippets/new" className="border p-2 rounded">
+            New
+          </Link>
+        </div>
+        <div className="flex flex-col gap-2">{renderedSnippets}</div>
+      </div>
     </React.Fragment>
   );
 }
