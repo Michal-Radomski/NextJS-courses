78f6ef106072cd32743061bdce11854673142e9d
Michal Radomski <michalra@op.pl>
2024-10-01 23:32:13 +0200

    NextJS course - commit 18

--- 02_NextJS/02_snippets/src/app/snippets/[id]/page.tsx
+++ 02_NextJS/02_snippets/src/app/snippets/[id]/page.tsx
@@ -6,11 +6,13 @@ interface SnippetShowPageProps {
   params: {
     id: string;
   };
+  searchParams: {};
 }
 
 export default async function SnippetShowPage(props: SnippetShowPageProps): Promise<JSX.Element> {
   // console.log("props:", props);
   const snippet = (await db.snippet.findFirst({
+    //  where: { id: Number(props.params.id) },
     where: { id: parseInt(props.params.id) },
   })) as Snippet;
 
