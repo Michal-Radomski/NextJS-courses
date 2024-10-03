c80382743d44d04cdd83c4c9b04079a89d4b3d41
Michal Radomski <michalra@op.pl>
2024-10-03 21:45:09 +0200

    NextJS course - commit 30

--- 02_NextJS/03_auth_discuss/src/app/page.tsx
+++ 02_NextJS/03_auth_discuss/src/app/page.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import { Button } from "@nextui-org/react";
 
 export default function Home(): JSX.Element {
-  return <React.Fragment>Home</React.Fragment>;
+  return (
+    <React.Fragment>
+      <div>
+        <Button>Click me!!!</Button>
+      </div>
+    </React.Fragment>
+  );
 }
