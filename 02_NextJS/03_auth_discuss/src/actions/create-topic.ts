f30ac6ad893c535559e180f342339cc31496b672
Michal Radomski <michalra@op.pl>
2024-10-05 00:56:56 +0200

    NextJS course - commit 45

--- 02_NextJS/03_auth_discuss/src/actions/create-topic.ts
+++ 02_NextJS/03_auth_discuss/src/actions/create-topic.ts
@@ -1,5 +1,28 @@
 "use server";
 
-export async function createTopic(): Promise<void> {
+import { z } from "zod";
+
+const createTopicSchema = z.object({
+  name: z
+    .string()
+    .min(3)
+    .regex(/^[a-z-]+$/, {
+      message: "Must be lowercase letters or dashes without spaces",
+    }),
+  description: z.string().min(10),
+});
+
+export async function createTopic(_formState: number, formData: FormData): Promise<number> {
+  const result = createTopicSchema.safeParse({
+    name: formData.get("name"),
+    description: formData.get("description"),
+  });
+
+  if (!result.success) {
+    console.log(result.error.flatten().fieldErrors);
+  }
+
+  return 10;
+
   // TODO: revalidate the homepage
 }
