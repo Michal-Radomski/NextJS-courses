8b8efa0166cc36dda7e50e7c3d26c4800c3606ce
Michal Radomski <michalra@op.pl>
2024-10-05 00:26:40 +0200

    NextJS course - commit 43

--- 02_NextJS/03_auth_discuss/src/app/page.tsx
+++ 02_NextJS/03_auth_discuss/src/app/page.tsx
@@ -1,32 +1,9 @@
 import React from "react";
-import { Button } from "@nextui-org/react";
 
-import { auth } from "@/auth";
-import * as actions from "@/actions";
-import Profile from "@/components/profile";
-
-export default async function Home(): Promise<JSX.Element> {
-  const session = await auth();
-
+export default function Home(): JSX.Element {
   return (
     <React.Fragment>
-      <div>
-        <form action={actions.signIn}>
-          <Button type="submit" color="primary">
-            Sign In
-          </Button>
-        </form>
-
-        <form action={actions.signOut}>
-          <Button type="submit" color="secondary">
-            Sign Out
-          </Button>
-        </form>
-
-        {session?.user ? <div>{JSON.stringify(session.user)}</div> : <div>Signed Out</div>}
-
-        <Profile />
-      </div>
+      <div>Home Page</div>
     </React.Fragment>
   );
 }
