d904444d4eec2968f3802634d28437ada0e507da
Michal Radomski <michalra@op.pl>
2024-10-05 23:25:12 +0200

    NextJS course - commit 48

--- 02_NextJS/03_auth_discuss/src/app/page.tsx
+++ 02_NextJS/03_auth_discuss/src/app/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { Divider } from "@nextui-org/react";
 
 import TopicCreateForm from "@/components/topics/topic-create-form";
+import TopicList from "@/components/topics/topic-list";
 
 export default function Home(): JSX.Element {
   return (
@@ -9,8 +11,11 @@ export default function Home(): JSX.Element {
         <div className="col-span-3">
           <h1 className="text-xl m-2">Top Posts</h1>
         </div>
-        <div>
+        <div className="border shadow py-3 px-2">
           <TopicCreateForm />
+          <Divider className="my-2" />
+          <h3 className="text-lg">Topics</h3>
+          <TopicList />
         </div>
       </div>
     </React.Fragment>
