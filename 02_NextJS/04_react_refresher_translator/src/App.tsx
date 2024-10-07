8c721fbcea0825427b03e2933a68cf40106fb632
Michal Radomski <michalra@op.pl>
2024-10-07 23:44:18 +0200

    NextJS course - commit 68

--- 02_NextJS/04_react_refresher_translator/src/App.tsx
+++ 02_NextJS/04_react_refresher_translator/src/App.tsx
@@ -2,8 +2,28 @@ import React from "react";
 
 import "./App.scss";
 
+const Exercise = (): JSX.Element => (
+  <>
+    <textarea autoFocus={true} />
+    <br />
+    <label htmlFor="disk_c">Disk usage C:</label>
+    <meter id="disk_c" value="2" min="0" max="10">
+      2 out of 10
+    </meter>
+    <br />
+    <label htmlFor="disk_d">Disk usage D:</label>
+    <meter id="disk_d" value="0.6">
+      60%
+    </meter>
+  </>
+);
+
 const App = (): JSX.Element => {
-  return <React.Fragment>App</React.Fragment>;
+  return (
+    <React.Fragment>
+      <Exercise />
+    </React.Fragment>
+  );
 };
 
 export default App;
