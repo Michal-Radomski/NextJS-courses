db0052df4e2aceabfc0740a3fd94d271c209854b
Michal Radomski <michalra@op.pl>
2024-10-08 22:45:12 +0200

    NextJS course - commit 73 - course done

--- 02_NextJS/05_TS_primer/index.ts
+++ 02_NextJS/05_TS_primer/index.ts
@@ -106,11 +106,11 @@ importantDates.push(new Date());
 console.log("importantDates:", importantDates);
 
 //* Tuples---------------
-const drink = {
-  color: "brown",
-  carbonated: true,
-  sugar: 40,
-};
+// const drink = {
+//   color: "brown",
+//   carbonated: true,
+//   sugar: 40,
+// };
 
 // Type alias
 type Drink = [string, boolean, number];
@@ -118,10 +118,13 @@ type Drink = [string, boolean, number];
 const pepsi: Drink = ["brown", true, 40];
 const sprite: Drink = ["clear", true, 40];
 const tea: Drink = ["brown", false, 0];
+console.log({ pepsi, sprite, tea });
 
 const carSpecs: [number, number] = [400, 3354];
+console.log({ carSpecs });
 
 const carStats = {
   horsepower: 400,
   weight: 3354,
 };
+console.log({ carStats });
