c4435e021cedf3715841ceec0245004150d5296b
Michal Radomski <michalra@op.pl>
2024-10-10 00:55:39 +0200

    Next_React course - commit 16

--- 03_Next_React/02_react_refresher/api/data/posts.ts
+++ 03_Next_React/02_react_refresher/api/data/posts.ts
@@ -3,7 +3,7 @@ import { promises as fs } from "fs";
 import { Post } from "../app";
 
 export async function getStoredPosts(): Promise<Post[]> {
-  const rawFileContent = await fs.readFile("posts.json", { encoding: "utf-8" });
+  const rawFileContent = await fs.readFile("api/posts.json", { encoding: "utf-8" });
   const data = JSON.parse(rawFileContent);
   const storedPosts = data.posts ?? [];
   return storedPosts;
