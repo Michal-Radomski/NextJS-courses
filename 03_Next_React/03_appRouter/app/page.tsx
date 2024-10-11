44dd6e9ec732f300dcf836617e16555fc179f1cf
Michal Radomski <michalra@op.pl>
2024-10-12 00:06:23 +0200

    Next_React course - commit 22

--- 03_Next_React/03_appRouter/app/page.tsx
+++ 03_Next_React/03_appRouter/app/page.tsx
@@ -1,14 +1,7 @@
-import Link from "next/link";
-
 export default function Home(): JSX.Element {
-  // console.log("Test"); //* Server component
   return (
     <main>
-      <h1>Welcome to this NextJS Course!</h1>
-      <p>🔥 Let&apos;s get started! 🔥</p>
-      <p>
-        <Link href={"/about"}>AboutPage</Link>
-      </p>
+      <h1 style={{ color: "white", textAlign: "center" }}>Time to get started!</h1>
     </main>
   );
 }
