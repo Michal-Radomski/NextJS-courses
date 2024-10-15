a1b078e75af5a18d2a6754ac00a63a768c5d2300
Michal Radomski <michalra@op.pl>
2024-10-15 22:23:46 +0200

    Next_React course - commit 45

--- 03_Next_React/04_routing&page_rendering/app/(content)/news/page.tsx
+++ 03_Next_React/04_routing&page_rendering/app/(content)/news/page.tsx
@@ -1,51 +1,73 @@
-"use client";
-
-import React from "react";
-
+//* V2
 // import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/news-list";
 
-export default function NewsPage(): JSX.Element {
-  const [isLoading, setIsLoading] = React.useState<boolean>(false);
-  const [error, setError] = React.useState<string>("");
-  const [news, setNews] = React.useState<News[]>([]);
+export default async function NewsPage(): Promise<JSX.Element> {
+  const response: Response = await fetch("http://localhost:8080/news");
 
-  React.useEffect(() => {
-    async function fetchNews(): Promise<void> {
-      setIsLoading(true);
-      const response: Response = await fetch("http://localhost:8080/news");
-
-      if (!response.ok) {
-        setError("Failed to fetch news.");
-        setIsLoading(false);
-      }
-
-      const news = (await response.json()) as News[];
-      setIsLoading(false);
-      setNews(news);
-    }
-
-    fetchNews();
-  }, []);
-
-  if (isLoading) {
-    return <p>Loading...</p>;
+  if (!response.ok) {
+    throw new Error("Failed tho fetch news.");
   }
 
-  if (error) {
-    return <p>{error}</p>;
-  }
-
-  let newsContent;
-
-  if (news) {
-    newsContent = <NewsList news={news} />;
-  }
+  const news = (await response.json()) as News[];
 
   return (
     <>
       <h1>News Page</h1>
-      {newsContent}
+      <NewsList news={news} />
     </>
   );
 }
+
+//* V1
+// "use client";
+
+// import React from "react";
+
+// // import { DUMMY_NEWS } from "@/dummy-news";
+// import NewsList from "@/components/news-list";
+
+// export default function NewsPage(): JSX.Element {
+//   const [isLoading, setIsLoading] = React.useState<boolean>(false);
+//   const [error, setError] = React.useState<string>("");
+//   const [news, setNews] = React.useState<News[]>([]);
+
+//   React.useEffect(() => {
+//     async function fetchNews(): Promise<void> {
+//       setIsLoading(true);
+//       const response: Response = await fetch("http://localhost:8080/news");
+
+//       if (!response.ok) {
+//         setError("Failed to fetch news.");
+//         setIsLoading(false);
+//       }
+
+//       const news = (await response.json()) as News[];
+//       setIsLoading(false);
+//       setNews(news);
+//     }
+
+//     fetchNews();
+//   }, []);
+
+//   if (isLoading) {
+//     return <p>Loading...</p>;
+//   }
+
+//   if (error) {
+//     return <p>{error}</p>;
+//   }
+
+//   let newsContent;
+
+//   if (news?.length) {
+//     newsContent = <NewsList news={news} />;
+//   }
+
+//   return (
+//     <>
+//       <h1>News Page</h1>
+//       {newsContent}
+//     </>
+//   );
+// }
