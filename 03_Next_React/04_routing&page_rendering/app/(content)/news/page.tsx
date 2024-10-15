102c34fc50882f4e8b4099b29992556829c1fdeb
Michal Radomski <michalra@op.pl>
2024-10-15 22:09:17 +0200

    Next_React course - commit 44

--- 03_Next_React/04_routing&page_rendering/app/(content)/news/page.tsx
+++ 03_Next_React/04_routing&page_rendering/app/(content)/news/page.tsx
@@ -1,11 +1,51 @@
-import { DUMMY_NEWS } from "@/dummy-news";
+"use client";
+
+import React from "react";
+
+// import { DUMMY_NEWS } from "@/dummy-news";
 import NewsList from "@/components/news-list";
 
 export default function NewsPage(): JSX.Element {
+  const [isLoading, setIsLoading] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string>("");
+  const [news, setNews] = React.useState<News[]>([]);
+
+  React.useEffect(() => {
+    async function fetchNews(): Promise<void> {
+      setIsLoading(true);
+      const response: Response = await fetch("http://localhost:8080/news");
+
+      if (!response.ok) {
+        setError("Failed to fetch news.");
+        setIsLoading(false);
+      }
+
+      const news = (await response.json()) as News[];
+      setIsLoading(false);
+      setNews(news);
+    }
+
+    fetchNews();
+  }, []);
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  let newsContent;
+
+  if (news) {
+    newsContent = <NewsList news={news} />;
+  }
+
   return (
     <>
       <h1>News Page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
