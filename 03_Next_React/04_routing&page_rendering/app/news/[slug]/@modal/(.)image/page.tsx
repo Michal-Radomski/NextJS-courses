f5543b08e8c7acf11a0da583372e5713ee145da4
Michal Radomski <michalra@op.pl>
2024-10-15 21:26:32 +0200

    Next_React course - commit 42

--- 03_Next_React/04_routing&page_rendering/app/news/[slug]/@modal/(.)image/page.tsx
+++ 03_Next_React/04_routing&page_rendering/app/news/[slug]/@modal/(.)image/page.tsx
@@ -1,9 +1,14 @@
-import { notFound } from "next/navigation";
+"use client";
+
+import { notFound, useRouter } from "next/navigation";
 import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
+import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 import { DUMMY_NEWS } from "@/dummy-news";
 
 export default function InterceptedImagePage({ params }: { params: Params }): JSX.Element {
+  const router: AppRouterInstance = useRouter();
+
   const newsItemSlug = params.slug as string;
   const newsItem = DUMMY_NEWS.find((newsItem: News) => newsItem.slug === newsItemSlug) as News;
 
@@ -13,7 +18,7 @@ export default function InterceptedImagePage({ params }: { params: Params }): JS
 
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
