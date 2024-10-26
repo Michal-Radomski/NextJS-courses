f5a530a95eaeb6e6e0502b3b65aa4c6c5808185a
Michal Radomski <michalra@op.pl>
2024-10-26 21:54:48 +0200

    Next_React course - commit 79

--- 03_Next_React/09_file_routing/pages/_app.tsx
+++ 03_Next_React/09_file_routing/pages/_app.tsx
@@ -1,7 +1,12 @@
 import type { AppProps } from "next/app";
 
 import "@/styles/globals.scss";
+import Layout from "@/components/layout/layout";
 
 export default function App({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
