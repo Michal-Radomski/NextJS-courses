e4669480f4f9a5c1d9d0450bb55c67f1426f63ef
Michal Radomski <michalra@op.pl>
2024-10-26 23:32:35 +0200

    Next_React course - commit 82

---
+++ 03_Next_React/10_pre-rendering/pages/index.tsx
@@ -0,0 +1,11 @@
+export default function HomePage(props: any): JSX.Element {
+  console.log("props:", props);
+
+  return (
+    <ul>
+      <li>Product 1</li>
+      <li>Product 2</li>
+      <li>Product 3</li>
+    </ul>
+  );
+}
