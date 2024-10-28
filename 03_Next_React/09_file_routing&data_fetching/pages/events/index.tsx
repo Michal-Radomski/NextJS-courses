297d6748ac276a4399470018dc1458b7a897230a
Michal Radomski <michalra@op.pl>
2024-10-28 23:59:10 +0100

    Next_React course - commit 93

--- 03_Next_React/09_file_routing&data_fetching/pages/events/index.tsx
+++ 03_Next_React/09_file_routing&data_fetching/pages/events/index.tsx
@@ -3,11 +3,14 @@ import { NextRouter, useRouter } from "next/router";
 
 import EventList from "@/components/events/event-list";
 import EventsSearch from "@/components/events/events-search";
-import { getAllEvents } from "@/dummy-data";
+// import { getAllEvents } from "@/dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 
-const AllEventsPage = (): JSX.Element => {
+const AllEventsPage = (props: { events: EventI[] }): JSX.Element => {
   const router: NextRouter = useRouter();
-  const events: EventI[] = getAllEvents();
+
+  // const events: EventI[] = getAllEvents();
+  const { events } = props;
 
   function findEventsHandler(year: string, month: string): void {
     const fullPath = `/events/${year}/${month}`;
@@ -22,5 +25,16 @@ const AllEventsPage = (): JSX.Element => {
     </React.Fragment>
   );
 };
+
+export async function getStaticProps(): Promise<any> {
+  const events: EventI[] = await getAllEvents();
+
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60,
+  };
+}
 
 export default AllEventsPage;
