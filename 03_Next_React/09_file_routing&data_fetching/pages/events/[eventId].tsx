da8cac261babb4ef017cef61bacaedf457399bed
Michal Radomski <michalra@op.pl>
2024-10-28 23:44:41 +0100

    Next_React course - commit 91

--- 03_Next_React/09_file_routing&data_fetching/pages/events/[eventId].tsx
+++ 03_Next_React/09_file_routing&data_fetching/pages/events/[eventId].tsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
-import { getEventById } from "../../dummy-data";
+// import { getEventById } from "../../dummy-data";
 import EventSummary from "../../components/event-detail/event-summary";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventContent from "../../components/event-detail/event-content";
 import ErrorAlert from "../../components/ui/error-alert";
+import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 
-const EventDetailPage = (): JSX.Element => {
-  const router = useRouter();
+const EventDetailPage = (props: { selectedEvent: EventI }): JSX.Element => {
+  // const router = useRouter();
+  // const eventId = router.query.eventId as string;
+  // const event: EventI = getEventById(eventId);
 
-  const eventId = router.query.eventId as string;
-  const event: EventI = getEventById(eventId);
+  const event: EventI = props.selectedEvent;
 
   if (!event) {
     return (
@@ -31,5 +33,29 @@ const EventDetailPage = (): JSX.Element => {
     </React.Fragment>
   );
 };
+
+export async function getStaticProps(context: { params: { eventId: string } }): Promise<any> {
+  const eventId = context.params.eventId;
+
+  const event: EventI = await getEventById(eventId);
+
+  return {
+    props: {
+      selectedEvent: event,
+    },
+    revalidate: 30,
+  };
+}
+
+export async function getStaticPaths(): Promise<any> {
+  const events: EventI[] = await getFeaturedEvents();
+
+  const paths = events.map((event: EventI) => ({ params: { eventId: event.id } }));
+
+  return {
+    paths: paths,
+    fallback: false,
+  };
+}
 
 export default EventDetailPage;
