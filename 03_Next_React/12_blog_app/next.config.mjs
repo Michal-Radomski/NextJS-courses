2f75fd4e0367cd6fceb26585e1017a488a7309c2
Michal Radomski <michalra@op.pl>
2024-11-03 22:47:05 +0100

    Next_React course - commit 126

--- 03_Next_React/12_blog_app/next.config.mjs
+++ 03_Next_React/12_blog_app/next.config.mjs
@@ -4,10 +4,8 @@ const nextConfig = {};
 export default nextConfig;
 
 //* .env file or this below:
-// // Importing PHASE_DEVELOPMENT_SERVER from next/constants
 // import { PHASE_DEVELOPMENT_SERVER } from 'next/constants';
 
-// // Exporting the configuration as a default export
 // export default (phase) => {
 //   if (phase === PHASE_DEVELOPMENT_SERVER) {
 //     return {
