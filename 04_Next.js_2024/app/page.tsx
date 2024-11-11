2cc19fc12ae6b26daaee06c1db328065c9a9254e
Michal Radomski <michalra@op.pl>
2024-11-11 23:59:57 +0100

    Next 2024 course - commit 04

--- 04_Next.js_2024/app/page.tsx
+++ 04_Next.js_2024/app/page.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 
 import variableColors from "./styles/_App.module.scss";
-
 const { textColor } = variableColors;
 
 export default function HomePage(): JSX.Element {
--- 04_Next.js_2024/tailwind.config.ts
+++ 04_Next.js_2024/tailwind.config.ts
@@ -4,9 +4,11 @@ export default {
   content: ["./pages/**/*.{js,ts,jsx,tsx,mdx}", "./components/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        sans: ["Poppins", "sans-serif"],
+      },
+      gridTemplateColumns: {
+        "70/30": "70% 28%",
       },
     },
   },
