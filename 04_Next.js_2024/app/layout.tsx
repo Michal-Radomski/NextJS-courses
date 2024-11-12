e7b4c22aab4efe28ef64fd06d034a7512cb2f9af
Michal Radomski <michalra@op.pl>
2024-11-13 00:58:34 +0100

    Next 2024 course - commit 06

--- 04_Next.js_2024/app/layout.tsx
+++ 04_Next.js_2024/app/layout.tsx
@@ -18,13 +18,8 @@ export default function MainLayout({
   return (
     <html lang="en">
       <head>
-        {/* Bootstrap CSS */}
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH"
-          crossOrigin="anonymous"
-        />
+        {/* TailwindCSS CDN */}
+        <script src="https://cdn.tailwindcss.com"></script>
       </head>
 
       <body className={""} suppressHydrationWarning={true}>
